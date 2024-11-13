0959387a6d62a00e63381dbcbe3873728bfd3748
ThanhToan <codehub.personal@gmail.com>
2024-11-13 14:19:03 +0700

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3001,
+    port: 3000,
     proxy: {
       "/api": {
         target: "https://ct449-project-2.onrender.com/",
